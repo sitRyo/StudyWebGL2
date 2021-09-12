a5927bcf5360c7cce6b510c72eb7b194420b3347
seriru <ry1126.stfe.sse@gmail.com>
2021-09-12 14:07:07 +0900

    feat: 頂点シェーダ, フラグメントシェーダを読み込めるようにwebpackの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,11 @@ module.exports = {
       {
         test: /\.ts$/i,
         use: ['ts-loader'],
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader']
       }
     ],
   },
