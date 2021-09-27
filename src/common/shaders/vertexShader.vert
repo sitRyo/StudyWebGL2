fab9b98b035c2dd4d3eacbd660e844ee04755382
seriru <ry1126.stfe.sse@gmail.com>
2021-09-27 20:27:55 +0900

    feat: ch3.10実装

--- src/common/shaders/vertexShader.vert
+++ src/common/shaders/vertexShader.vert
@@ -4,16 +4,24 @@ precision mediump float;
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 uniform mat4 uNormalMatrix;
+uniform vec3 uLightDirection;
+uniform vec4 uLightAmbient;
+uniform vec4 uLightDiffuse;
+uniform vec4 uMaterialDiffuse;
 
 in vec3 aVertexPosition;
 in vec3 aVertexNormal;
 
-out vec3 vNormal;
-out vec3 vEyeVector;
+out vec4 vVertexColor;
 
 void main(void) {
-  vec4 vertex = uModelViewMatrix * vec4(aVertexPosition, 1.0);
-  vNormal = vec3(uNormalMatrix * vec4(aVertexPosition, 1.0)); // 法線は補完されてフラグメントシェーダーに
-  vEyeVector = -vec3(vertex.xyz);
+  vec3 N = vec3(uNormalMatrix * vec4(aVertexNormal, 1.0));
+  vec3 L = normalize(uLightDirection);
+  float lambertTerm = dot(N, -L);
+
+  vec4 Ia = uLightAmbient;
+  vec4 Id = uMaterialDiffuse * uLightDiffuse * lambertTerm;
+
+  vVertexColor = vec4(vec3(Ia + Id), 1.0);
   gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
 }
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,13 @@ import vertShader from './common/shaders/vertexShader.vert';
 import fragShader from './common/shaders/fragmentShader.frag';
 import { isGLint } from './common/lib/typeGuards';
 import { mat4 } from 'gl-matrix';
-import { AttributeKind, Model } from './common/utils/types';
-import { vertices, indices } from './info'; // ch03で使用する頂点とインデックス情報
 import { AttLocation } from './common/utils/types';
 
 const Utils = new utils();
 let gl: WebGL2RenderingContext = null;
-let sphereVAO: WebGLVertexArrayObject | null = null;
-let sphereIndicesBuffer: WebGLBuffer | null = null;
+let vao: WebGLVertexArrayObject | null = null;
+let indicesBuffer: WebGLBuffer | null = null;
 let shininess = 10;
-let clearColor = [0.9, 0.9, 0.9, 1];
 let lightColor = [1, 1, 1, 1];
 let lightAmbient = [0.03, 0.03, 0.03, 1];
 let lightSpecular = [1, 1, 1, 1];
@@ -25,9 +22,7 @@ let program: WebGLProgram | null;
 let projectionMatrix = mat4.create();
 let modelViewMatrix = mat4.create();
 let normalMatrix = mat4.create();
-let angle = 0;
-let lastTime: number | null = null;
-let wireframe = false;
+let indices: number[];
 
 // programオブジェクト内のシェーダへのインデックスを格納する
 const attLocation: AttLocation = {}; 
@@ -68,43 +63,51 @@ const initProgram = (): void => {
   attLocation.uProjectionMatrix = gl.getUniformLocation(program, 'uProjectionMatrix');
   attLocation.uModelViewMatrix = gl.getUniformLocation(program, 'uModelViewMatrix');
   attLocation.uNormalMatrix = gl.getUniformLocation(program, 'uNormalMatrix');
-  attLocation.uMaterialAmbient = gl.getUniformLocation(program, 'uMaterialAmbient');
-  attLocation.uMaterialDiffuse = gl.getUniformLocation(program, 'uMaterialDiffuse');
-  attLocation.uMaterialSpecular = gl.getUniformLocation(program, 'uMaterialSpecular');
-  attLocation.uShininess = gl.getUniformLocation(program, 'uShininess');
+  attLocation.uLightDirection = gl.getUniformLocation(program, 'uLightDirection');
   attLocation.uLightAmbient = gl.getUniformLocation(program, 'uLightAmbient');
   attLocation.uLightDiffuse = gl.getUniformLocation(program, 'uLightDiffuse');
-  attLocation.uLightSpecular = gl.getUniformLocation(program, 'uLightSpecular');
-  attLocation.uLightDirection = gl.getUniformLocation(program, 'uLightDirection');
+  attLocation.uMaterialDiffuse = gl.getUniformLocation(program, 'uMaterialDiffuse');
 }
 
 const initLights = () => {
-  // 光源
-  gl.uniform4fv(attLocation.uLightDiffuse, lightColor); // 拡散反射光
-  gl.uniform4fv(attLocation.uLightAmbient, lightAmbient); // 環境光
-  gl.uniform4fv(attLocation.uLightSpecular, lightSpecular); // 鏡面反射光
-  gl.uniform3fv(attLocation.uLightDirection, lightDirection); // 方向
-  // マテリアル
-  gl.uniform4fv(attLocation.uMaterialDiffuse, materialDiffuse); // 拡散反射光
-  gl.uniform4fv(attLocation.uMaterialAmbient, materialAmbient); // 環境光
-  gl.uniform4fv(attLocation.uMaterialSpecular, materialSpecular); // 鏡面反射光
-  gl.uniform1f(attLocation.uShininess, shininess); // 光沢度
+  gl.uniform3fv(attLocation.uLightDirection, [0, 0, -1]);
+  gl.uniform4fv(attLocation.uLightAmbient, [0.01, 0.01, 0.01, 1]);
+  gl.uniform4fv(attLocation.uLightDiffuse, [0.5, 0.5, 0.5, 1]);
+  gl.uniform4f(attLocation.uMaterialDiffuse, 0.1, 0.5, 0.8, 1);
 }
 
 const initBuffers = () => {
-  // コードの見通しが非常に悪くなるため
-  // verticesとindicesは./info.tsからインポートする
-  const normals = Utils.calculateNormals(vertices, indices);
-  
+  const vertices = [
+    -20, -8, 20, // 0
+    -10, -8, 0,  // 1
+    10, -8, 0,   // 2
+    20, -8, 20,  // 3
+    -20, 8, 20,  // 4
+    -10, 8, 0,   // 5
+    10, 8, 0,    // 6
+    20, 8, 20    // 7
+  ];
+
+  indices = [
+    0, 5, 4,
+    1, 5, 0,
+    1, 6, 5,
+    2, 6, 1,
+    2, 7, 6,
+    3, 7, 2
+  ];
+
   // Create VAO
-  sphereVAO = gl.createVertexArray();
+  vao = gl.createVertexArray();
 
   // Bind VAO
-  gl.bindVertexArray(sphereVAO);
+  gl.bindVertexArray(vao);
+
+  const normals = Utils.calculateNormals(vertices, indices);
 
   // Vertices
-  const sphereVerticesBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, sphereVerticesBuffer);
+  const verticesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   // Configure VAO instructions
   if (isGLint(attLocation.aVertexPosition)) {
@@ -113,8 +116,8 @@ const initBuffers = () => {
   }
 
   // 法線
-  const sphereNormalsBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, sphereNormalsBuffer);
+  const normalsBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
   if (isGLint(attLocation.aVertexNormal)) {
     gl.enableVertexAttribArray(attLocation.aVertexNormal);
@@ -122,8 +125,8 @@ const initBuffers = () => {
   }
 
   // インデックス
-  sphereIndicesBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphereIndicesBuffer);
+  indicesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
   // Clean
@@ -133,14 +136,15 @@ const initBuffers = () => {
 }
 
 const draw = (): void => {
+  const { width, height } = gl.canvas;
+
   // シーンのクリア
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.viewport(0, 0, width, height);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  mat4.perspective(projectionMatrix, 45, gl.canvas.width / gl.canvas.height, 0.1, 10000);
+  mat4.perspective(projectionMatrix, 45, width / height, 0.1, 10000);
   mat4.identity(modelViewMatrix);
-  mat4.translate(modelViewMatrix, modelViewMatrix, [0, 0, -1.5]);
-  mat4.rotate(modelViewMatrix, modelViewMatrix, angle * Math.PI / 180, [0, 1, 0]);
+  mat4.translate(modelViewMatrix, modelViewMatrix, [0, 0, -40]);
 
   mat4.copy(normalMatrix, modelViewMatrix);
   mat4.invert(normalMatrix, normalMatrix);
@@ -152,12 +156,11 @@ const draw = (): void => {
 
   try {
     // Bind
-    gl.bindVertexArray(sphereVAO);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphereIndicesBuffer);
+    gl.bindVertexArray(vao);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
 
-    const type = wireframe ? gl.LINES : gl.TRIANGLES;
     // Draw
-    gl.drawElements(type, indices.length, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
     // Clean
     gl.bindVertexArray(null);
@@ -170,18 +173,8 @@ const draw = (): void => {
   }
 }
 
-const animate = () => {
-  let timeNow = new Date().getTime();
-  if (lastTime) {
-    const elapsed = timeNow - lastTime;
-    angle += (90 * elapsed) / 1000.0;
-  }
-  lastTime = timeNow;
-}
-
 const render = () => {
   window.requestAnimationFrame(render); // ブラウザにアニメーションを行わせる
-  animate();
   draw();
 }
 
@@ -190,63 +183,6 @@ const init = (): void => {
   initBuffers();
   initLights();
   render();
-
-  initControls();
-}
-
-const initControls = () => {
-  Utils.configureControls({
-    'Light Color': {
-      value: Utils.denormalizeColor(lightColor),
-      onChange: v => gl.uniform4fv(attLocation.uLightDiffuse, Utils.normalizeColor(v))
-    },
-    'Light Ambient Term': {
-      value: lightAmbient[0],
-      min: 0, max: 1, step: 0.01,
-      onChange: v => gl.uniform4fv(attLocation.uLightAmbient, [v, v, v, 1])
-    },
-    ...['Translate X', 'Translate Y', 'Translate Z'].reduce((result, name, i) => {
-      result[name] = {
-        value: lightDirection[i],
-        min: -10, max: 10, step: -0.1,
-        onChange(v, state) {
-          gl.uniform3fv(attLocation.uLightDirection, [
-            -state['Translate X'],
-            -state['Translate Y'],
-            state['Translate Z']
-          ]);
-        }
-      };
-      return result;
-    }, {}),
-    'Sphere Color': {
-      value: Utils.denormalizeColor(materialDiffuse),
-      onChange: v => gl.uniform4fv(attLocation.uMaterialDiffuse, Utils.normalizeColor(v))
-    },
-    'Material Ambient Term': {
-      value: materialAmbient[0],
-      min: 0, max: 1, step: 0.01,
-      onChange: v => gl.uniform4fv(attLocation.uMaterialAmbient, [v, v, v, 1])
-    },
-    'Material Specular Term': {
-      value: materialSpecular[0],
-      min: 0, max: 1, step: 0.01,
-      onChange: v => gl.uniform4fv(attLocation.uMaterialSpecular, [v, v, v, 1])
-    },
-    Shininess: {
-      value: shininess,
-      min: 0, max: 50, step: 0.1,
-      onChange: v => gl.uniform1f(attLocation.uShininess, v)
-    },
-    Background: {
-      value: Utils.denormalizeColor(clearColor),
-      onChange: v => gl.clearColor(Utils.normalizeColor(v)[0], Utils.normalizeColor(v)[1], Utils.normalizeColor(v)[2], 1)
-    },
-    Wireframe: {
-      value: wireframe,
-      onChange: v => wireframe = v
-    }
-  })
 }
 
 // html実行時にinitを実行する
