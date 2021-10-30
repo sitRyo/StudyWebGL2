2ff20b446e2d85b259e8f33352730c5d36988794
seriru <ry1126.stfe.sse@gmail.com>
2021-10-30 14:03:33 +0900

    feat: index.tsにチャプターを追加

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+/* ch04 */
+// import { init } from './ch04/ch04_01';
+// import { init } from './ch04/ch04_02';
+// import { init } from './ch04/ch04_03';
+// import { init } from './ch04/ch04_04';
+// import { init } from './ch04/ch04_05';
+// import { init } from './ch04/ch04_06';
+
+/* ch05 */
+// import { init } from './ch05/ch05_01';
+// import { init } from './ch05/ch05_02';
+// import { init } from './ch05/ch05_03;
 import { init } from './ch05/ch05_04';
 
 init();
